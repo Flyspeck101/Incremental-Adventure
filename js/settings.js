08ca59354e0f55ec89371e4c34915c149893339b
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-03-22 17:50:11 +1100

    Create settings.js

---
+++ js/settings.js
@@ -0,0 +1,6 @@
+var desusify = (function(){
+  if (document.getElementById("title").innerHTML == "Incremental Adventure") {
+    document.getElementById("title").innerHTML = "Art Project";
+  } else {
+    document.getElementById("title").innerHTML = "Incremental Adventure";
+})
