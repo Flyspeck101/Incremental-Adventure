8f8b6b23f61eb6e6847cf1b11a7119a7fddfd77f
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-03-22 20:28:50 +1100

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,26 @@
+var maxHealth = 100;
+var health = 100;
+var healingSpeed = 1;
 
+function healthBoost(extra) {
+  maxHealth += extra;
+}
+
+function naturalHeal() {
+  health += healingSpeed;
+  if (health > maxHealth) {
+    health = maxHealth;
+  }
+}
+
+function setHealingSpeed(newValue) {
+  healingSpeed = newValue;
+}
+
+function healingSpeedBoost(extra) {
+  healingSpeed += extra;
+}
+
+
+var money = 0;
+var moneyRate = 1;
