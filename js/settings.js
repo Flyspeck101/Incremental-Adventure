b9aa57c8d3e3215ab81a5f65ac032f45fe871b33
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-03-22 18:16:44 +1100

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,1 @@
-var desusify = (function(){
-  if (document.getElementById("title").innerHTML == "Incremental Adventure") {
-    document.getElementById("title").innerHTML = "Art Project";
-  } else {
-    document.getElementById("title").innerHTML = "Incremental Adventure";
-  }
-})
+var desusify=(function(){if(document.getElementById("title").innerHTML=="Incremental Adventure"){document.getElementById("title").innerHTML = "Art Project";}else{document.getElementById("title").innerHTML="Incremental Adventure";}})
